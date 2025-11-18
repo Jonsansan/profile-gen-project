4d822019a92ff22fed9411825d2261ae8b39573c
Jon Sanchez Santiago <127Jsanchezsantiago@frhsd.com>
2025-11-18 13:59:19 -0500

    =99-

--- js folder/script.js
+++ js folder/script.js
@@ -1,5 +1,24 @@
 /** @format */
 
-document.addEventListener("DOMContentLoaded"), function () {
-    const 
-};
+document.addEventListener("DOMContentLoaded", function () {
+  const nameInput = document.getElementById("nameInput");
+  const ageInput = document.getElementById("ageInput");
+  const colorInput = document.getElementById("colorInput");
+  const favoritePetSelect = document.getElementById("favoritePet");
+  const favoritePetOptions = ["Dog", "Cat", "Fish"];
+
+  nameInput.addEventListener("input", function () {
+    console.log("Textbox value changed to: " + nameInput.value);
+  });
+
+  ageInput.addEventListener("input", function () {
+    console.log("Textbox value changed to: " + ageInput.value);
+  });
+
+  colorInput.addEventListener("click", function () {
+    console.log("Textbox value changed to: " + colorInput.value);
+  });
+  favoritePetSelect.addEventListener("change", function () {
+    console.log("Selected favorite pet: " + favoritePetSelect.value);
+  });
+});
