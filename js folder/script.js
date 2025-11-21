f2f6d7830b82fe6054f2bdd7fa17aa8549fb54c1
Jon Sanchez Santiago <127Jsanchezsantiago@frhsd.com>
2025-11-21 13:21:00 -0500

    tyuiou89u98

--- js folder/script.js
+++ js folder/script.js
@@ -4,21 +4,14 @@ document.addEventListener("DOMContentLoaded", function () {
   const nameInput = document.getElementById("nameInput");
   const ageInput = document.getElementById("ageInput");
   const colorInput = document.getElementById("colorInput");
-  const favoritePetSelect = document.getElementById("favoritePet");
-  const favoritePetOptions = ["Dog", "Cat", "Fish"];
+  const submitBtn = document.getElementById("submitBtn");
+  const output = document.getElementById("output");
 
-  nameInput.addEventListener("input", function () {
-    console.log("Textbox value changed to: " + nameInput.value);
-  });
+  submitBtn.addEventListener("click", function () {
+    const name = nameInput.value;
+    const age = ageInput.value;
+    const color = colorInput.value;
 
-  ageInput.addEventListener("input", function () {
-    console.log("Textbox value changed to: " + ageInput.value);
-  });
-
-  colorInput.addEventListener("click", function () {
-    console.log("Textbox value changed to: " + colorInput.value);
-  });
-  favoritePetSelect.addEventListener("change", function () {
-    console.log("Selected favorite pet: " + favoritePetSelect.value);
+    output.textContent = `Name: ${name}, Age: ${age}, Favorite Color: ${color}`;
   });
 });
