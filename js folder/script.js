63e1c8848d2e60c2c55695bc535ef6e1124a0f57
Jon Sanchez Santiago <127Jsanchezsantiago@frhsd.com>
2025-11-26 10:54:41 -0500

    update working hobby

--- js folder/script.js
+++ js folder/script.js
@@ -6,12 +6,29 @@ document.addEventListener("DOMContentLoaded", function () {
   const colorInput = document.getElementById("colorInput");
   const submitBtn = document.getElementById("submitBtn");
   const output = document.getElementById("output");
+  const pet = document.querySelectorAll('input[type="radio"]');
+  const hobby = document.querySelectorAll('input[type="checkbox"]');
 
   submitBtn.addEventListener("click", function () {
     const name = nameInput.value;
     const age = ageInput.value;
     const color = colorInput.value;
 
-    output.textContent = `Name: ${name}, Age: ${age}, Favorite Color: ${color}`;
+    let favpet = "";
+    let hobbies = [];
+
+    for (let i = 0; i < pet.length; i++) {
+      if (pet[i].checked) {
+        favpet = pet[i].value;
+      }
+    }
+
+    for (let i = 0; i < hobby.length; i++) {
+      if (hobby[i].checked) {
+        hobbies.push(hobby[i].value);
+      }
+    }
+
+    output.textContent = `Name: ${name}, Age: ${age}, Favorite Color: ${color}, Favorite Pet: ${favpet}, Hobbies: ${hobbies.join(", ")}`;
   });
 });
